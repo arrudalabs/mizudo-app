575004d5ac7a2681284e21622c04bf23cefa4bcf
Maximillian Arruda <dearrudam@gmail.com>
2022-05-18 21:29:16 -0300

    Fixed icon images

--- webapp/src/assets/icons/tmp/renameIcons.sh
+++ webapp/src/assets/icons/tmp/renameIcons.sh
@@ -1,1 +1,6 @@
-for f in $(ls *x*.png);do SIZE=$(echo "$f" | sed 's/^.*x//g' | sed 's/\..*$//g'); mv -f $f ./../icon-${SIZE}x${SIZE}.png ; done;
+# https://maskable.app/editor
+for f in $(ls *x*.png)
+do
+  SIZE=$(echo "$f" | sed 's/^.*x//g' | sed 's/\..*$//g');
+  mv -f $f ./../icon-${SIZE}x${SIZE}.png
+done
