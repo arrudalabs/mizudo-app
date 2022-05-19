d699859876b090804753e50595278a79357fee05
Maximillian Arruda <dearrudam@gmail.com>
2022-05-18 23:16:45 -0300

    Improving CI for Angular project[SKIP_CI]

--- webapp/src/app/app.component.spec.ts
+++ webapp/src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
@@ -5,6 +6,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
       imports: [
         RouterTestingModule
       ],
@@ -30,6 +32,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('mizudo-app app is running!');
+    expect(compiled.querySelector('.toolbar-title')?.textContent).toContain('Mizu-do');
   });
 });
