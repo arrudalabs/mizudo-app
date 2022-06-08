065ec0bae4070dddd57fbe83b94c2955506959d9
Maximillian Arruda <dearrudam@gmail.com>
2022-06-08 16:37:48 -0300

    FEAT: auto-login and auto-logout implemented [SKIP_CD]

--- webapp/src/app/auth/auth.guard.ts
+++ webapp/src/app/auth/auth.guard.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable, tap } from 'rxjs';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable, take, tap, switchMap, of } from 'rxjs';
 import { SessionService } from '../services/session.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-
-  constructor(private session:SessionService,
-              private router:Router){}
+  constructor(private session: SessionService, private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-
-    return this.session.isLogged()
-      .pipe(tap(isAuthenticated=>{
-        if(!isAuthenticated){
-          this.router.navigate(["/","login"]);
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.session.isLogged().pipe(
+      take(1),
+      switchMap((isLogged) => {
+        return !isLogged ? this.session.autologin() : of(isLogged);
+      }),
+      tap((isLogged) => {
+        if (!isLogged) {
+          this.router.navigate(['/', 'login']);
         }
-      }));
+      })
+    );
   }
-
 }
--- webapp/src/app/services/session.service.ts
+++ webapp/src/app/services/session.service.ts
@@ -2,14 +2,28 @@ import { Credentials } from './../model/Credentials';
 import { MizudoService } from './mizudo.service';
 import { Token } from './../model/Token';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map, Observable, of, switchMap, tap } from 'rxjs';
+import {
+  BehaviorSubject,
+  from,
+  ignoreElements,
+  map,
+  Observable,
+  of,
+  switchMap,
+  tap,
+} from 'rxjs';
+import { decodeJwt } from 'jose';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SessionService {
 
+  static TOKEN_KEY = 'mizudoToken';
+
   private _token = new BehaviorSubject<Token | null>(null);
+
+  private activeAutoLogoutTimer: any;
 
   constructor(private mizudoService: MizudoService) {}
 
@@ -26,14 +40,70 @@ export class SessionService {
   }
 
   auth({ username, password }: Credentials): Observable<Token> {
-    return this.mizudoService
-      .getToken({ username, password })
-      .pipe(tap((token) => {
-        this._token.next(token);
-      }));
+    return this.mizudoService.getToken({ username, password }).pipe(
+      tap((token) => {
+        this.storeToken(token);
+      })
+    );
+  }
+
+  storeToken(token: Token) {
+    localStorage.setItem(SessionService.TOKEN_KEY, JSON.stringify(token));
+    const durationInSeconds = token.expires_in;
+    this.defineTokenAndAutoLogout(durationInSeconds, token);
+  }
+
+  private defineTokenAndAutoLogout(durationInSeconds: number, token: Token) {
+    this.autologout(durationInSeconds);
+    this._token.next(token);
+  }
+
+  autologout(durationIsSeconds: number) {
+    this.destroyAutoLogoutTimeout();
+    console.log(`auto-logout in ${durationIsSeconds} second(s)`);
+    this.activeAutoLogoutTimer = setTimeout(async () => {
+      console.log('calling auto-logout...');
+      await this.logOut();
+    }, durationIsSeconds * 1000);
+  }
+
+  private destroyAutoLogoutTimeout() {
+    if (this.activeAutoLogoutTimer) {
+      console.log(`destroying auto-logout...`);
+      clearTimeout(this.activeAutoLogoutTimer);
+    }
+  }
+
+  autologin(): Observable<boolean> {
+    const storedData = localStorage.getItem(SessionService.TOKEN_KEY);
+
+    if (!storedData) {
+      return of(false);
+    }
+    return of(JSON.parse(storedData) as Token).pipe(
+      map((storedToken) => {
+        const payload = decodeJwt(storedToken.access_token);
+
+        if (!payload.iat || !payload.exp) {
+          return false;
+        }
+
+        const expirationTime = new Date(payload.exp * 1000);
+
+        if (expirationTime <= new Date()) {
+          return false;
+        }
+
+        this.defineTokenAndAutoLogout((payload.exp - payload.iat) ,storedToken);
+
+        return true;
+      })
+    );
   }
 
   async logOut() {
+    localStorage.removeItem(SessionService.TOKEN_KEY);
+    this.destroyAutoLogoutTimeout();
     this._token.next(null);
   }
 }
